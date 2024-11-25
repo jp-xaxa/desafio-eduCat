c571fc14a57edd4f61c9ad82dbdac25b7cfa45ef
Joao Pedro <joaopedrolamim@yahoo.com.br>
2024-11-25 18:38:25 -0300

    Ajustes dos arquivos e estilização global

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ThemeProvider } from "styled-components"
+import GlobalStyles from "./styles/global"
 
-createRoot(document.getElementById('root')!).render(
+import theme from "./styles/theme"
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  </StrictMode>
 )
