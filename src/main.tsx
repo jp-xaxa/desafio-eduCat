b772aaf9c8ae1e5c973ef0c864b39462f25dd60c
Joao Pedro <joaopedrolamim@yahoo.com.br>
2024-11-25 18:46:47 -0300

    Desenvolvimento do sistema de rotas da aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 import { ThemeProvider } from "styled-components"
 import GlobalStyles from "./styles/global"
 
 import theme from "./styles/theme"
+import { Router } from "./router"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <Router />
     </ThemeProvider>
   </StrictMode>
 )
