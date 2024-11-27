237248d7ebe36a1c73f2b2c1120c0d207b84cd55
Joao Pedro <joaopedrolamim@yahoo.com.br>
2024-11-27 17:14:39 -0300

    Criado arquivos da página de detalhe do curso

---
+++ src/pages/Details/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/Header"
+
+import { Container } from "./styles"
+
+export function Details() {
+  return (
+    <Container>
+      <Header />
+      <h1>Página de detalhes</h1>
+    </Container>
+  )
+}
---
+++ src/pages/Details/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-areas:
+    "header "
+    " content ";
+  grid-template-rows: max-content auto;
+`
